fb6afea4a0035e9a68a962bf69c0df65c83380f7
ValdemarHermannss <valdi04h@gmail.com>
2024-09-27 14:54:45 +0200

    Door work
    More work on the door and the door script.

--- Assets/_Scripts/AnimatedWallTest.cs
+++ Assets/_Scripts/AnimatedWallTest.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AnimatedWallTest : MonoBehaviour
 {
-    public Vector3 minScale = new Vector3(1f, 0.5f, 1f);
-    public Vector3 maxScale = new Vector3(1f, 1.5f, 1f);
-    public float speed = 2f;
+    private bool isCollidingWithWall = false;
+    private bool isCollidingWithMovingDoor = false;
+    
+    public Vector3 minScale = new Vector3(1f, 0.5f, 1f); //Min Scale
+    public Vector3 maxScale = new Vector3(1f, 1.5f, 1f); //Max Scale
+    public float speed = 2f; //Speed
 
     private bool isGrowing = true;
+    private bool isCrushing = false;
 
     // Update is called once per frame
     void Update()
@@ -29,5 +34,62 @@ public class AnimatedWallTest : MonoBehaviour
             if (Vector3.Distance(transform.localScale, minScale) < 0.01f)
                 isGrowing = true;
         }
+
+        isCrushing = !isGrowing;
     }
-}
+    private void OnCollisionEnter2D(Collision2D collision) // Checks if it collides with the player and the position of the player when it collides.
+    {
+        if (collision.gameObject.CompareTag("Walls"))
+        {
+            isCollidingWithWall = true;
+            Debug.Log("Colliding with a wall");
+        }
+        
+        if (collision.gameObject.CompareTag("MovingDoor"))
+        {
+            isCollidingWithMovingDoor = true;
+            Debug.Log("Colliding with the moving door");
+        }
+
+        if (isCollidingWithWall && isCollidingWithMovingDoor)
+        {
+            DataStore.Collectibles.Remove(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+    private void OnCollissionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Walls"))
+            {
+                isCollidingWithWall = false;
+            }
+
+            if (collision.gameObject.CompareTag("MovingDoor"))
+            {
+                isCollidingWithMovingDoor = false;
+            }
+        }
+        
+        /* if (collision.gameObject.tag == "MovingDoor" && "Walls")
+             DataStore.Collectibles.Remove(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("GameOverScene");
+
+
+
+
+        /*if (collision.gameObject.tag == "Snail")
+        {
+            Vector2 playerPosition = collision.transform.position;
+
+            Vector2 wallBottomEdge = new Vector2(transform.position.x, transform.position.y - (transform.localScale.y / 2));
+            
+            
+            if (isCrushing && playerPosition.y < wallBottomEdge.y) // If the player is under the bottom edge of the wall it will m
+            {
+                DataStore.Collectibles.Remove(SceneManager.GetActiveScene().name);
+                SceneManager.LoadScene("GameOverScene");
+            }
+        } */
+    }
+
+
